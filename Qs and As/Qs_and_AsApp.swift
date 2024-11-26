c623c6a9d9188bdaa8750e625202972e53db83b6
Fulton Browne <fultonbrowne@pm.me>
2024-11-26 09:10:33 -0600

    Initial Commit

---
+++ Qs and As/Qs_and_AsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Qs_and_AsApp.swift
+//  Qs and As
+//
+//  Created by Fulton Browne on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Qs_and_AsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
